14f652b46e230bbe1d90f7ffc83bafb89d6bd0ec
Fortunso88 <ture_schuldiner@hotmail.it>
2012-09-07 14:38:24 +0200

    upload

--- src/progettoing/MediatorFrame.java
+++
@@ -1,9 +0,0 @@
-
-package progettoing;
-
-public interface MediatorFrame {
-    
-    abstract AboutFrame getAboutFrame();
-    abstract ApriFrame getApriFrame();
-    abstract SalvaFrame getSalvaFrame(String tf);
-}
